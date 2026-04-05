6a44b9c030e1fe9ea809ee624237b8498d907764
KIWALYA NICHOLAS <nkiwalya1@gmail.com>
2026-04-05 18:19:42 +0300

    first commit
    pushed repo to git

---
+++ data.js
@@ -0,0 +1,12 @@
+gender: "Male"
+id: 1
+image: "https://dragonball-api.com/characters/goku_normal.webp"
+ki
+: 
+"60.000.000"
+maxKi
+: "90 Septillion"
+name
+: "Goku"
+race
+: "Saiyan"
---
+++ fetch.js
@@ -0,0 +1,23 @@
+async function fetchData() {
+   try {
+     const charName = document.getElementById("char-name").value.toLowerCase()
+    const response = await fetch(`https://dragonball-api.com/api/characters?name=${charName}`)
+    if (!response.ok || response.length === 0) {
+        document.getElementById('person').innerText =  `we could not find the resource`
+        throw new Error("we could not find the resource"); 
+    }
+    const data = await response.json()
+    console.log(data);
+    console.log(data[0].name);
+    document.getElementById('person').style.display = `block`
+    document.getElementById("db-character").src = data[0].image
+    document.getElementById('name').innerText = data[0].name
+    document.getElementById('gender').innerText = data[0].gender
+    document.getElementById('race').innerText = data[0].race
+    document.getElementById('ki').innerText = data[0].ki
+    document.getElementById('maxki').innerText = data[0].maxKi
+}
+ catch (error) {
+    console.error(error);
+   }
+}
